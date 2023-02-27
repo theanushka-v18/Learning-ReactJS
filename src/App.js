6656f8a071b5ae44d5bb849f3f4c6a12fc0827fc
Anushka Verma <anushkaverma779@gmail.com>
2023-02-27 11:53:23 +0530

    Introduction to ReactJS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
 function App() {
+  let name = "Anushka";
   return (
     <div className="App-header">
-      Hello
+      App Component
+      My name is {name}
+      <Demo />
+    </div>
+  );
+}
+
+function Demo() {
+  let learning = "ReactJS";
+  return (
+    <div className="App-header">
+      Demo Component
+      I am learning {learning}
     </div>
   );
 }
